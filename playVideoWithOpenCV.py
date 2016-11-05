8074d4eca36ad3ae71f827bff938112716290eaa
Karunanidhi Devanidhi <karunchillz@users.noreply.github.com>
2016-11-05 17:36:24 -0600

    Update playVideoWithOpenCV.py

--- playVideoWithOpenCV.py
+++ playVideoWithOpenCV.py
@@ -1,23 +1,16 @@
+import numpy as np
 import cv2
-import sys
 
-try:
-    vidFile = cv2.VideoCapture(sys.argv[1])
-except:
-    print "problem opening input stream"
-    sys.exit(1)
-if not vidFile.isOpened():
-    print "capture stream not open"
-    sys.exit(1)
+cap = cv2.VideoCapture('vtest.avi')
 
-nFrames = int(vidFile.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)) # one good way of namespacing legacy openCV: cv2.cv.*
-print "frame number: %s" %nFrames
-fps = vidFile.get(cv2.cv.CV_CAP_PROP_FPS)
-print "FPS value: %s" %fps
+while(cap.isOpened()):
+    ret, frame = cap.read()
 
-ret, frame = vidFile.read() # read first frame, and the return code of the function.
-while ret:  # note that we don't have to use frame number here, we could read from a live written file.
-    print "yes"
-    cv2.imshow("frameWindow", frame)
-    cv2.waitKey(int(1/fps*1000)) # time to wait between frames, in mSec
-    ret, frame = vidFile.read() # read next frame, get next return code
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

a1b5dc27ca2de117aefb1a2c40f30396836ac74a
Karunanidhi Devanidhi <karunchillz@users.noreply.github.com>
2016-11-06 00:13:16 -0600

    Update vid.cpp

--- vid.cpp
+++ vid.cpp
@@ -1,18 +1,32 @@
-#include<opencv2\highgui\highgui.hpp>
-#include<iostream>
-using namespace std;
-using namespace cv;
-int main(void)
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include "opencv2/opencv.hpp"
+int main(int argc, char** argv)
 {
-       Mat frame;
-       VideoCapture cap("Intel.mp4");   //open the capture for video file
-       int totalframe = cap.get(CV_CAP_PROP_FRAME_COUNT);
-  // get total number of frames in video
-       while (waitKey(30) != 27 && totalframe>0)
-  //wait 30 milliseconds and check for esc key
-       {
-              cap >> frame; //save captured image to frame variable
-              imshow("Canvas", frame); //show image on window named Camera
-              totalframe--;
-       }
+cvNamedWindow("Example3", CV_WINDOW_AUTOSIZE);
+
+//CvCapture* capture = cvCreateFileCapture("20051210-w50s.flv");
+CvCapture* capture = cvCreateFileCapture("Intel.avi");
+/* if(!capture)
+    {
+        std::cout <<"Video Not Opened\n";
+        return -1;
+    }*/
+IplImage* frame = NULL;
+
+while(1) {
+
+    frame = cvQueryFrame(capture);
+    //std::cout << "Inside loop\n";
+    if (!frame)
+        break;
+    cvShowImage("Example3", frame);
+    char c = cvWaitKey(33);
+    if (c == 27) break;
+}
+cvReleaseCapture(&capture);
+cvDestroyWindow("Example3");
+std::cout << "Hello!";
+return 0;
 }

76d5851fb49c827d4b17b8c3fdde2b30ff41465a
karunchillz <karun.chillz@outlook.com>
2016-11-06 07:45:04 +0000

    changes

---
+++ custom_video.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(2)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('Intel.avi',fourcc, 20.0, (640,480))
+
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out.write(frame)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()

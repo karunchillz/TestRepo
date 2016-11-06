ee2410b6246ae4b3ca5c277cf335317efb4e3095
Karunanidhi Devanidhi <karunchillz@users.noreply.github.com>
2016-11-05 21:58:53 -0600

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -1,22 +1,32 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <opencv2/core/core.hpp>
 #include "opencv2/opencv.hpp"
-
-using namespace cv;
-
-int main(int, char**)
+int main(int argc, char** argv)
 {
-VideoCapture cap("Intel.avi"); // open the default camera
-//Video Capture cap(path_to_video); // open the video file
-if(!cap.isOpened())  // check if we succeeded
-    return -1;
+cvNamedWindow("Example3", CV_WINDOW_AUTOSIZE);
 
-namedWindow("Video",1);
-for(;;)
-{
-    Mat frame;
-    cap >> frame; // get a new frame from camera        
-    imshow("Video", frame);
-    if(waitKey(30) >= 0) break;
+//CvCapture* capture = cvCreateFileCapture("20051210-w50s.flv");
+CvCapture* capture = cvCreateFileCapture("Intel.avi");
+/* if(!capture)
+    {
+        std::cout <<"Video Not Opened\n";
+        return -1;
+    }*/
+IplImage* frame = NULL;
+
+while(1) {
+
+    frame = cvQueryFrame(capture);
+    //std::cout << "Inside loop\n";
+    if (!frame)
+        break;
+    cvShowImage("Example3", frame);
+    char c = cvWaitKey(33);
+    if (c == 27) break;
 }
-// the camera will be deinitialized automatically in VideoCapture destructor
+cvReleaseCapture(&capture);
+cvDestroyWindow("Example3");
+std::cout << "Hello!";
 return 0;
 }
